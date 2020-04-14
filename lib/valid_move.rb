296184c66b1ca515866c4e110304d50f76cedfaa
Yuliya Yurkevich <juliana.ny2008@gmail.com>
2020-04-14 18:27:49 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,13 +1,7 @@
 # code your #valid_move? method here
 
 
-#def valid_move?(board, index)
-# def position_taken?(board, index)
- #  board[index] == " " || board[index] == "" || board[index] == nil
- #  #board[index] =="X" || board[index] =="O"
-# end
-#puts position_taken?(board,index) && index.between?(0,8)
- #end
+
 
 
 def position_taken? (board, index)
