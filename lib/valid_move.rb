3ed5c14f62c3e256075b411dab806e30dca0dbd4
Yuliya Yurkevich <juliana.ny2008@gmail.com>
2020-04-14 17:55:18 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -4,7 +4,7 @@
 def valid_move?(board, index)
   def position_taken?(board, index)
    board[index] == " " || board[index] == "" || board[index] == nil
-   board[index] =="X" || board[index] =="O"
+   #board[index] =="X" || board[index] =="O"
  end
  
  
