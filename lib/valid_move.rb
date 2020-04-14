af3e2ae6f936709db22dd1990abd5a8a56a74419
Yuliya Yurkevich <juliana.ny2008@gmail.com>
2020-04-14 18:30:11 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -13,7 +13,7 @@ def position_taken? (board, index)
 end
 
 def valid_move?(board, index)
-  if !position_taken?(board, index) && (index).between?(0,8)
+  if position_taken?(board, index) && (index).between?(0,8)
     return true
   else 
     return false
